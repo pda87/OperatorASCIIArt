92a47cb510acbee34f0e38607236093b76f505ed
Pete Arden <pete.davidarden@gmail.com>
2016-11-10 17:59:15 +0000

    Added "Nice Version"
    The "CodeGolf" version is not very nice to read.

--- c#/OperatorASCIIArt.cs
+++ c#/OperatorASCIIArt.cs
@@ -8,6 +8,94 @@ namespace A.CodeGolf
 {
   class OperatorASCIIArt
   {
+    #region Nice Version
+
+    //Nice version
+    public string GetASCIIArt(string character, int size)
+    {
+      string output = "";
+
+      switch (character)
+      {
+        case "+":
+
+          for (int i = 0; i < size; i++)
+          {
+            if (i == size / 2)
+            {
+              output += Repeat("+", size);
+            }
+            else
+            {
+              output += "+".PadLeft(size - size / 2, ' ').PadRight(size - size / 2, ' ') + "\r\n";
+            }
+          }
+
+          return output;
+        case "=":
+          return Repeat("=", size) + Repeat("=", size);
+        case "-":
+          return Repeat("-", size);
+        case "/":
+
+          for (int i = size; i > 0; i--)
+          {
+            output += "/".PadLeft(i) + "\r\n";
+          }
+
+          return output;
+        case "x":
+          int correction = 0;
+
+          string[] rows = new string[size];
+
+          for (int i = size; i > 0; i--)
+          {
+            if (size - correction - correction < 0)
+            {
+              output += "x".PadLeft(correction + 1) + "\r\n";
+              break;
+            }
+
+            string row = string.Join("", Enumerable.Repeat(" ", correction))
+              + "x"
+              + string.Join("", Enumerable.Repeat(" ", size - correction - correction)) + "x" + "\r\n";
+
+            output += row;
+            correction++;
+            rows[correction] = row;
+
+            if (i == size / 2)
+            {
+              break;
+            }
+          }
+
+          for (int i = rows.Length - 1; i > 0; i--)
+          {
+            if (string.IsNullOrEmpty(rows[i]))
+            {
+              continue;
+            }
+            output += rows[i];
+          }
+
+          return output;
+        default:
+          return "";
+      }
+    }
+
+    private string Repeat(string repeatedCharacter, int numberOfCharacters)
+    {
+      return string.Join("", Enumerable.Repeat(repeatedCharacter, numberOfCharacters)) + "\r\n";
+    }
+
+    #endregion Nice Version
+
+    #region Code Golf Version
+
+    //Code Golf version
     public string A(string s, int n)
     {
       string O = "";
@@ -77,4 +165,7 @@ namespace A.CodeGolf
       }
     }
   }
+
+  #endregion Code Golf Version
+
 }
